c1708787b54d887c1701728b44acbae9adc77f67
Michael Vertuli <vertuli@factual.com>
2019-10-05 22:18:11 -0700

    Added postgres engine.

--- website/website/settings.py
+++ website/website/settings.py
@@ -78,7 +78,16 @@ WSGI_APPLICATION = "website.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {"default": {"ENGINE": "django.db.backends.postgresql_psycopg2"}}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "website",
+        "USER": "django",
+        "HOST": "208.97.141.39",
+        "PORT": "5432",
+        "CONN_MAX_AGE": 1,
+    }
+}
 
 
 # Password validation
