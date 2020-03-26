e15177ddcd5dfbab173e41669d583334ecb9be30
Michael Vertuli <vertuli@factual.com>
2020-03-26 11:06:24 -0700

    Restructuring as EB compatible flask app.

--- update-webserver.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This overwrites the existing Apache Webserver /var/www/html content of the
-# EC2 instance with the contents of the local html folder in this directory.
-
-# Name of host defined in ~/.ssh/ with credentials to the EC2 webserver.
-HOST="aws_personal"
-
-scp -r html $HOST:/var/www/
