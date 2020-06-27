d8f23956b4fc5ae983388bff70e6afd84cfe4400
vertuli <michael@vertuli.com>
2020-06-27 14:51:16 -0700

    Updated flask app.

--- application.py
+++ application.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html')
+
+
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
